a474a93f393e002d0faee385de295256cfbab556
Shamsiddin <xamroqulovsh7@gmail.com>
2025-04-24 17:22:08 +0500

    Add public css and js

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,8 +1,8 @@
 class ViewsController {
     constructor() {
-        this.main = async (req, res) => res.render("index"),
-        this.register = async (req, res) => res.render("register"),
-        this.login = async (req, res) => res.render("login")
+        this.main = async (req, res) => res.render("index", {title: "Asosiy Sahifa", customJs: "/js/index.js", customCss: "/css/index.css"}),
+        this.register = async (req, res) => res.render("register", {title: "Asosiy Sahifa", customJs: "/js/index.js", customCss: "/css/index.css"}),
+        this.login = async (req, res) => res.render("login", {title: "Asosiy Sahifa", customJs: "/js/index.js", customCss: "/css/index.css"})
     }
 }
 
